55cfd85c31bb637757242da072ab1b1ab90e8757
Xandebrabe <alexandre.souza@sou.inteli.edu.br>
2024-03-24 21:10:25 -0300

    feat: adding readme

--- obj/Debug/net8.0/metrics_application.AssemblyInfo.cs
+++ obj/Debug/net8.0/metrics_application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("metrics_application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+004fd5dcafd86ac1e932674e9ec5d86780a26ce3")]
 [assembly: System.Reflection.AssemblyProductAttribute("metrics_application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("metrics_application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
